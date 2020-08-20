eaf1e09cdbd9856717aad03d815a925346da9dac
alifavaldo <alifavaldo@gmail.com>
2020-08-20 13:59:36 +0700

    finish footer and animation and i want to work on google

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,6 +4,8 @@ import landingPage from 'json/landingPage.json';
 import Hero from 'parts/Hero';
 import MostPicked from 'parts/MostPicked'
 import Categories from 'parts/Categories'
+import Testimony from 'parts/Testimony'
+import Footer from 'parts/Footer'
 
 export default class LandingPage extends Component {
   constructor(props) {
@@ -19,6 +21,8 @@ export default class LandingPage extends Component {
           refMostPicked={this.refMostPicked}
           data={landingPage.mostPicked} />
         <Categories data={landingPage.categories} />
+        <Testimony data={landingPage.testimonial} />
+        <Footer />
       </>
     );
   }
