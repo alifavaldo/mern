804cb6f43dd55bfef7ad47690cc7250096d2fcba
alifavaldo <alifavaldo@gmail.com>
2020-09-04 11:28:54 +0700

    menambahkan input file, masih belajar karna pengen kerja di google

---
+++ src/elements/Form/InputFile/index.js
@@ -0,0 +1,59 @@
+import React, { useRef } from 'react'
+import propTypes from 'prop-types';
+import "./index.scss";
+
+export default function File(props) {
+  const {
+    value,
+    placeholder,
+    name,
+    accept,
+    prepend,
+    append,
+    outerClassName,
+    inputClassName,
+  } = props;
+
+  const refInputFile = useRef(null);
+  return (
+    <div className={["input-text mb-3", outerClassName].join(" ")}>
+      <div className="input-group">
+        {prepend && (
+          <div className="input-group-prepend bg-gray-900">
+            <span className="input-group-text">{prepend}</span>
+          </div>
+        )}
+        <input
+          accept={accept}
+          ref={refInputFile}
+          name={name}
+          className="d-none"
+          value={value}
+          onChange={props.onChange}
+          type="file"
+        />
+        {append && (
+          <div className="input-group-append bg-gray-900">
+            <span className="input-group-text">{append}</span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+File.defaultProps = {
+  placeholder: "Browse a file..",
+};
+
+File.propTypes = {
+  name: propTypes.string.isRequired,
+  accept: propTypes.string.isRequired,
+  value: propTypes.oneOfType([propTypes.number, propTypes.string]).isRequired,
+  onChange: propTypes.func.isRequired,
+  prepend: propTypes.oneOfType([propTypes.number, propTypes.string]).isRequired,
+  append: propTypes.oneOfType([propTypes.number, propTypes.string]).isRequired,
+  placeholder: propTypes.string,
+  outerClassName: propTypes.string,
+  inputClassName: propTypes.string,
+}
