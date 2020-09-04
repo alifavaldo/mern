495f6aeb30f9f2c3337a0de1487827f5ea44585f
alifavaldo <alifavaldo@gmail.com>
2020-09-04 11:11:18 +0700

    menyelesaikan detail page dan menambahkan form input text

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import "assets/scss/style.scss";
 import LandingPage from 'pages/LandingPage';
+import DetailsPage from 'pages/DetailsPage';
+import Checkout from 'pages/Checkout';
 
 function App() {
   return (
     <div className="App">
       <Router >
-        <Route path="/" component={LandingPage} />
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/properties/:id" component={DetailsPage} />
+        <Route path="/checkout" component={Checkout} />
       </Router>
     </div>
   );
---
+++ src/elements/Stepper/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import propTypes from 'prop-types'
+
+export default function Stepper(props) {
+  const { steps, initialSteps } = props;
+  const stepKeys = Object.keys(steps);
+
+  const [CurrentStep, setCurrentStep] = useState(
+    stepKeys.indexOf(initialSteps) > -1 ? initialSteps : stepKeys[0]
+  );
+
+  const totalStep = stepKeys.length;
+  const indexStep = stepKeys.indexOf(CurrentStep);
+
+  function prevStep() {
+    if (+indexStep > 0) setCurrentStep(stepKeys[indexStep - 1]);
+  }
+
+  function nextStep() {
+    if (+indexStep < totalStep) setCurrentStep(stepKeys[indexStep + 1]);
+  }
+  return <>{props.children(prevStep, nextStep, CurrentStep, steps)}</>;
+}
+
+Stepper.propTypes = {
+  data: propTypes.object.isRequired,
+  initialSteps: propTypes.string,
+};
